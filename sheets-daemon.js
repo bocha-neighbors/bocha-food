3b583b0453c7e6a0d25eecf2c297d1798c38a1f7
Zachary Smith <billyzacsmith@gmail.com>
2015-12-29 12:44:08 -0700

    The function replaceDatabase removes all items from the database and then inserts the items from the google spreadsheet.
    To use this function:
    - Set condition in the the .where() to be true for all items.
    - Set replaceDatabase as the callback for Tabletop init.

--- sheets-daemon.js
+++ sheets-daemon.js
@@ -1,7 +1,7 @@
 var knex = require('./db/knex')
 var Tabletop = require('tabletop')
 
-function Item() {
+function Items() {
   return knex('items')
 }
 
@@ -9,7 +9,7 @@ var public_spreadsheet_url =
   'https://docs.google.com/spreadsheets/d/1xMW98m2-Y8rrbmACA9l1fUT5jb4tKvrRBCSWvH28guw/pubhtml?gid=1526253182&single=true'
 
 Tabletop.init({ key: public_spreadsheet_url,
-                callback: updateDatabase,
+                callback: showData,
                 simpleSheet: true,
                 parseNumbers: false,
                 debug: false
@@ -20,7 +20,7 @@ function updateDatabase(data, tabletop) {
   var length = data.length
   for (var i=0; i<length; i++) {
     var itemFromSheet = data[i]
-    Item().where({ vendor_item_id: itemFromSheet.vendor_item_id })
+    Items().where({ vendor_item_id: itemFromSheet.vendor_item_id })
     .update({
       vendor: itemFromSheet.vendor,
       item_type: itemFromSheet.item_type,
@@ -38,8 +38,53 @@ function updateDatabase(data, tabletop) {
     .catch(function(err) {
       console.log(err)
     })
+    .then(function(results) {
+      console.log('Finish updating.')
+      console.log('Results: ', results)
+    })
   } // for loop
 } // updateDatabase declaration
+
+function replaceDatabase(data, tabletop) {
+  // First, delete all rows in database.
+  // Put a condition in the .where() that selects all the rows.
+  console.log('Clearing all items from the database.')
+  Items().del().where({
+    vendor: 'Silver Monkeys'
+  })
+  .then(function(results) {
+    console.log(results)
+  })
+
+  // Then, insert all items from the spreadsheet into the database
+  console.log('Replacing database with google spreadsheet items.')
+  var length = data.length
+  for (var i=0; i<length; i++) {
+    var itemFromSheet = data[i]
+    Items()
+    .insert({
+      vendor: itemFromSheet.vendor,
+      item_type: itemFromSheet.item_type,
+      vendor_item_id: itemFromSheet.vendor_item_id,
+      company: itemFromSheet.company,
+      item_description: itemFromSheet.item_description,
+      quantity: parseFloat(itemFromSheet.quantity),
+      unit: itemFromSheet.unit,
+      price: parseFloat(itemFromSheet.price),
+      unit_price: parseFloat(itemFromSheet.unit_price),
+      typical_origin: itemFromSheet.typical_origin,
+      wholesale_unit: itemFromSheet.wholesale_unit,
+      wholesale_unit_price: parseFloat(itemFromSheet.wholesale_unit_price)
+    }, 'id')
+    .catch(function(err) {
+      console.log(err)
+    })
+    .then(function(results) {
+      console.log('Finish updating.')
+      console.log('Results: ', results)
+    })
+  } // for loop
+} // replaceDatabase declaration
 
 function showData(data, tabletop) {
   console.log(data)
