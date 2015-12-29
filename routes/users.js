2d3c90f74b4d8c851d59a512166b6f3af0b1a81f
Zachary Smith <billyzacsmith@gmail.com>
2015-12-29 14:32:56 -0700

    Add users table. Add the basic outline of the user routes.

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+var Express = require('express')
+var router = Express.Router()
+var knex  = require('../db/knex')
+
+function Users() {
+  return knex('users')
+}
+
+router.get('/', function(request, response) {
+  Users().select()
+  .then(function(results) {
+    var jsonResponse = {}
+    jsonResponse.links = { "self": "http://somehost.com/users" }
+    jsonResponse.data = results
+    response.json(jsonResponse)
+  })
+})
+
+router.get('/:id', function(request, response) {
+  Users().select()
+  .where('id', request.params.id).first()
+  .then(function(results) {
+    var jsonResponse = {}
+    jsonResponse.links = { "self": "http://somehost.com/users" }
+    jsonResponse.data = results
+    response.json(jsonResponse)
+  })
+})
+
+router.post('/', function(request, response) {
+  Users().insert({
+    
+  }, 'id')
+  .then(function(results) {
+    var jsonResponse = {}
+    jsonResponse.links = { "self": "http://somehost.com/users" }
+    jsonResponse.data = results
+    response.json(jsonResponse)
+  })
+})
+
+router.put('/:id', function(request, response) {
+  Users().where('id', request.params.id).update({
+
+  })
+  .then(function(results) {
+    var jsonResponse = {}
+    jsonResponse.links = { "self": "http://somehost.com/users" }
+    jsonResponse.data = results
+    response.json(jsonResponse)
+  })
+})
+
+router.delete('/:id', function(request, response) {
+  Users().where('id', request.params.id).del()
+  .then(function(results) {
+    var jsonResponse = {}
+    jsonResponse.links = { "self": "http://somehost.com/users" }
+    jsonResponse.data = results
+    response.json(jsonResponse)
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var knex = require('./db/knex')
 var bodyParser = require('body-parser')
 
 var items = require('./routes/items')
+var users = require('./routes/users')
 
 var app = Express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api/v1/items', items)
+app.use('/api/v1/users', users)
 
 app.get('/', function(request, response) {
   console.log(request)
