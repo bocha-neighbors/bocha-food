338201d0bab56532f510895fc37f0a83f1441d2b
Zachary Smith <billyzacsmith@gmail.com>
2015-12-29 10:37:11 -0700

    Pulls items from google spreadsheet. Updates items that are currently in the database with data from the spreadsheet.
    The script won't handle the following cases:
    - New items added to the spreadsheet.
    - Items deleted from the spreadsheet.
    
    So right now, the script does not yet have full functionality. Will need to return to this later, if we want to keep the google sheet as part of the system.

--- sheets-daemon.js
+++ sheets-daemon.js
@@ -5,60 +5,42 @@ function Item() {
   return knex('items')
 }
 
-
-
 var public_spreadsheet_url =
   'https://docs.google.com/spreadsheets/d/1xMW98m2-Y8rrbmACA9l1fUT5jb4tKvrRBCSWvH28guw/pubhtml?gid=1526253182&single=true'
+
 Tabletop.init({ key: public_spreadsheet_url,
                 callback: updateDatabase,
                 simpleSheet: true,
+                parseNumbers: false,
                 debug: false
 })
+
 function updateDatabase(data, tabletop) {
-  // For each item in the data
-  for (var i=0; i<data.length; i++) {
-    var item = data[i]
-    // Check if the item exists in the DB
-    Item().where(
-      { vendor_item_id: item.vendor_item_id }
-    )
-    .then(function(result){
-      if (result[0])
-        // If the item already exists, update the database with (potentially) new values
-        console.log('Found ' + result[0].vendor_item_id)
-      else
-      {
-        // If does not exist insert a new row
-        console.log('Not found')
-        Item().insert( {
-          vendor: item.vendor,
-          item_type: item.item_type,
-          vendor_item_id: item.vendor_item_id,
-          company: item.company,
-          item_description: item.item_description,
-          quantity: parseFloat(item.quantity),
-          unit: item.unit,
-          price: parseFloat(item.price),
-          unit_price: parseFloat(item.unit_price),
-          typical_origin: item.typical_origin,
-          wholesale_unit: item.wholesale_unit,
-          wholesale_unit_price: parseFloat(item.wholesale_unit_price)
-        }, 'id')
-        .then(function(result){
-          console.log('Inserted item', result)
-        }) // end Item().insert()
-      } // end else
-    }) // end .then()
-    .catch(function(err) {
-      console.log('There was an err with #' + i)
-      console.error(err)
+  console.log('Updating existing items')
+  var length = data.length
+  for (var i=0; i<length; i++) {
+    var itemFromSheet = data[i]
+    Item().where({ vendor_item_id: itemFromSheet.vendor_item_id })
+    .update({
+      vendor: itemFromSheet.vendor,
+      item_type: itemFromSheet.item_type,
+      vendor_item_id: itemFromSheet.vendor_item_id,
+      company: itemFromSheet.company,
+      item_description: itemFromSheet.item_description,
+      quantity: parseFloat(itemFromSheet.quantity),
+      unit: itemFromSheet.unit,
+      price: parseFloat(itemFromSheet.price),
+      unit_price: parseFloat(itemFromSheet.unit_price),
+      typical_origin: itemFromSheet.typical_origin,
+      wholesale_unit: itemFromSheet.wholesale_unit,
+      wholesale_unit_price: parseFloat(itemFromSheet.wholesale_unit_price)
     })
-
-
-
-
-
-
-
-  } // for
+    .catch(function(err) {
+      console.log(err)
+    })
+  } // for loop
 } // updateDatabase declaration
+
+function showData(data, tabletop) {
+  console.log(data)
+}
